e324d07ee50f5f85a590a103dc63ee139d59cc8c
smithone <wyxh0422@163.com>
2025-09-08 18:04:14 +0800

    第三次提交

--- api/app.py
+++ api/app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route("/")  # 关键点：直接在路由中写 .json
 def index():
     return "Hello world!"
+
+@app.route("/test")  # 关键点：直接在路由中写 .json
+def index():
+    return "Hello world! This is a test!"
 @app.route("/data.json")  # 关键点：直接在路由中写 .json
 def get_data():
     return jsonify({
