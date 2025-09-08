6974b5503a808015bf49e7fb302503d5c51fd03d
smithone <wyxh0422@163.com>
2025-09-08 17:54:15 +0800

    第二次提交

--- api/app.py
+++ api/app.py
@@ -2,6 +2,10 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
+@app.route("/")  # 关键点：直接在路由中写 .json
+def index():
+    return "Hello world!"
 @app.route("/data.json")  # 关键点：直接在路由中写 .json
 def get_data():
     return jsonify({
