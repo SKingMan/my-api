93be8140f101d9cc583f0706c0326e8d1036e610
smithone <wyxh0422@163.com>
2025-09-08 17:40:03 +0800

    第一次提交

---
+++ api/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/data.json")  # 关键点：直接在路由中写 .json
+def get_data():
+    return jsonify({
+        "message": "This is a .json endpoint",
+        "data": [1, 2, 3]
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
